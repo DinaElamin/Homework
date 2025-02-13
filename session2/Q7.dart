84b90a81fdea69c1a8f9a2d9ab770e9846bcad31
Dina Elamin <dinelamin62@gmail.com>
2025-02-13 21:37:04 +0200

    session2done

---
+++ session2/Q7.dart
@@ -0,0 +1,16 @@
+/*If-Else with Grades
+Objective: Practice conditional statements with integer values.
+Instructions:
+- Create an integer variable marks.
+- If marks is 70 or higher, print 'Passed', otherwise print 'Failed' */
+import 'dart:io';
+
+void main() {
+  print('enter your marks');
+  int marks = int.parse(stdin.readLineSync()!);
+  if (marks >= 70) {
+    print('passed');
+  } else {
+    print('failed');
+  }
+}
